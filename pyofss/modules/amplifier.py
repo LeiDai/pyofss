330cf79c92b4c512f6c7e18104d51fcdb4fcbf8b
David Bolt <djb@curvedthoughts.com>
2012-12-11 13:55:41 +0000

    Edit remaining pyofss modules to conform to PEP8.

--- pyofss/modules/amplifier.py
+++ pyofss/modules/amplifier.py
@@ -1,9 +1,8 @@
 
-# ==============================================================================
 """
     Copyright (C) 2011, 2012  David Bolt
 
-	 This file is part of pyofss.
+    This file is part of pyofss.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,52 +17,51 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-# ==============================================================================
 
 from scipy import power, sqrt
 from pyofss.field import fft, ifft
-# ==============================================================================
-class Amplifier( object ):
-   """
-   :param string name: Name of this module
-   :param double gain: Amount of (logarithmic) gain. *Unit: dB*
-   :param double power: Average power level to target
 
-   Simple amplifier provides gain but no noise
-   """
-   # ===========================================================================
-   def __init__( self, name = "amplifier", gain = None, power = None ):
 
-      self.name = name
+class Amplifier(object):
+    """
+    :param string name: Name of this module
+    :param double gain: Amount of (logarithmic) gain. *Unit: dB*
+    :param double power: Average power level to target
 
-      if( (gain is None) and (power is None) ):
-         print "Amplifier has not been supplied with gain or power parameter"
-      if( (gain is not None) and (power is not None) ):
-         print "Amplifier given both gain and power parameters"
+    Simple amplifier provides gain but no noise
+    """
+    def __init__(self, name="amplifier", gain=None, power=None):
 
-      self.gain = gain
-      self.power = power
-   # ===========================================================================
-   def __call__( self, domain, field ):
+        self.name = name
 
-      # Convert field to spectral domain:
-      self.field = fft( field )
-      # ========================================================================
-      if( self.gain is not None ):
-         # Calculate linear gain from logarithmic gain (G_dB -> G_linear)
-         G = power( 10, 0.1 * self.gain )
-         sqrt_G = sqrt( G )         
+        if (gain is None) and (power is None):
+            print "Amplifier has not been given gain or power parameter"
+        if (gain is not None) and (power is not None):
+            print "Amplifier given both gain and power parameters"
 
-         if( domain.channels > 1 ):
-            self.field[0] *= sqrt_G
-            self.field[1] *= sqrt_G
-         else:
-            self.field *= sqrt_G
-      # ========================================================================
-      # TODO: implement power control
-      if( self.power is not None ):
-         pass    
-      # ========================================================================
-      # convert field back to temporal domain:
-      return ifft( self.field ) 
-# ==============================================================================
+        self.gain = gain
+        self.power = power
+
+        self.field = None
+
+    def __call__(self, domain, field):
+
+        # Convert field to spectral domain:
+        self.field = fft(field)
+
+        if self.gain is not None:
+            # Calculate linear gain from logarithmic gain (G_dB -> G_linear)
+            G = power(10, 0.1 * self.gain)
+            sqrt_G = sqrt(G)
+
+            if domain.channels > 1:
+                self.field[0] *= sqrt_G
+                self.field[1] *= sqrt_G
+            else:
+                self.field *= sqrt_G
+
+        if self.power is not None:
+            pass
+
+        # convert field back to temporal domain:
+        return ifft(self.field)
--- pyofss/modules/cw.py
+++ pyofss/modules/cw.py
@@ -1,9 +1,8 @@
 
-# ==============================================================================
 """
     Copyright (C) 2011, 2012  David Bolt
 
-	 This file is part of pyofss.
+    This file is part of pyofss.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,43 +17,42 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-# ==============================================================================
 
 from numpy import pi
 from scipy import sqrt, exp
-# ==============================================================================
-class Cw( object ):
-   """
-   :param string name: Name of this module
-   :param double peak_power: Peak power of the CW source
-   :param double offset_nu: Offset frequency
-   :param double initial_phase: Initial phase
-   :param Uint channel: Channel sets the field to be modified
 
-   Generate a continuous wave (CW) source. Add this to appropriate field.
-   """
-   # ===========================================================================
-   def __init__( self, name = "cw", peak_power = 0.0, offset_nu = 0.0,
-                 initial_phase = 0.0, channel = 0 ):
 
-      self.name = name
-      self.peak_power = peak_power
-      self.offset_nu = offset_nu
-      self.initial_phase = initial_phase
-      self.channel = channel
-   # ===========================================================================
-   def __call__( self, domain, field ):
+class Cw(object):
+    """
+    :param string name: Name of this module
+    :param double peak_power: Peak power of the CW source
+    :param double offset_nu: Offset frequency
+    :param double initial_phase: Initial phase
+    :param Uint channel: Channel sets the field to be modified
 
-      self.field = field
-      # ========================================================================
-      phase = self.initial_phase
-      phase += 2.0 * pi * self.offset_nu * domain.t
-      shape = sqrt( self.peak_power ) * exp( -(1j * phase) )
-      # ========================================================================
-      if( domain.channels > 1 ):
-         self.field[ self.channel ] += shape
-      else:
-         self.field += shape
-      # ========================================================================
-      return self.field
-# ==============================================================================
+    Generate a continuous wave (CW) source. Add this to appropriate field.
+    """
+    def __init__(self, name="cw", peak_power=0.0, offset_nu=0.0,
+                 initial_phase=0.0, channel=0):
+
+        self.name = name
+        self.peak_power = peak_power
+        self.offset_nu = offset_nu
+        self.initial_phase = initial_phase
+        self.channel = channel
+
+        self.field = None
+
+    def __call__(self, domain, field):
+        self.field = field
+
+        phase = self.initial_phase
+        phase += 2.0 * pi * self.offset_nu * domain.t
+        shape = sqrt(self.peak_power) * exp(-(1j * phase))
+
+        if domain.channels > 1:
+            self.field[self.channel] += shape
+        else:
+            self.field += shape
+
+        return self.field
